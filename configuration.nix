ec5a508f5608f50c741cc4a6123bba7aeaa63f38
Joaquín Guerra <45147327+warbacon@users.noreply.github.com>
2025-04-18 15:29:44 +0200

    Request es-ES in firefox

--- configuration.nix
+++ configuration.nix
@@ -184,6 +184,7 @@
       DisableFirefoxStudies = true;
       OverrideFirstRunPage = "";
       OverridePostUpdatePage = "";
+      RequestedLocales = "es-ES";
       ExtensionSettings = {
         "es-es@dictionaries.addons.mozilla.org" = {
           install_url = "addons.mozilla.org/firefox/downloads/latest/diccionario-de-español-españa/latest.xpi";
