c2103bf63cf86db53094a084755283a230e9d174
Joaquín Guerra <45147327+warbacon@users.noreply.github.com>
2025-04-22 11:54:24 +0200

    perf: use firefox-bin

--- modules/graphical/firefox.nix
+++ modules/graphical/firefox.nix
@@ -1,6 +1,8 @@
+{ pkgs, ... }:
 {
   home-manager.users.warbacon.programs.firefox = {
     enable = true;
+    package = pkgs.firefox-bin;
     languagePacks = [ "es-ES" ];
     policies = {
       DisableTelemetry = true;
