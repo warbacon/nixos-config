edcffd5183a6a0faf7cd98d82fa928131fe03647
Joaquín Guerra <45147327+warbacon@users.noreply.github.com>
2025-04-23 12:33:49 +0200

    perf(neovim): install LSPs globally

--- modules/base/programs/neovim.nix
+++ modules/base/programs/neovim.nix
@@ -1,33 +1,31 @@
 { pkgs, ... }:
 {
+  # LSPs and tools
+  environment.systemPackages = with pkgs; [
+    astro-language-server
+    basedpyright
+    bash-language-server
+    clang-tools
+    docker-compose-language-service
+    dockerfile-language-server-nodejs
+    emmet-language-server
+    intelephense
+    lua-language-server
+    markdownlint-cli
+    nixd
+    nixfmt-rfc-style
+    shellcheck-minimal
+    stylua
+    tailwindcss-language-server
+    vscode-langservers-extracted
+    vtsls
+    yaml-language-server
+  ];
+
   programs.neovim = {
     enable = true;
     defaultEditor = true;
     withRuby = false;
     withPython3 = false;
-  };
-
-  home-manager.users.warbacon.programs.neovim = {
-    enable = true;
-    extraPackages = with pkgs; [
-      astro-language-server
-      basedpyright
-      bash-language-server
-      clang-tools
-      docker-compose-language-service
-      dockerfile-language-server-nodejs
-      emmet-language-server
-      intelephense
-      lua-language-server
-      markdownlint-cli
-      nixd
-      nixfmt-rfc-style
-      shellcheck-minimal
-      stylua
-      tailwindcss-language-server
-      vscode-langservers-extracted
-      vtsls
-      yaml-language-server
-    ];
   };
 }
