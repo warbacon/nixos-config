f53228830211909d9e9481dcd7acbab9e8ae7908
Joaquín Guerra <45147327+warbacon@users.noreply.github.com>
2025-04-23 13:49:24 +0200

    fix(shell): make code command work in fish wsl

--- modules/base/shell.nix
+++ modules/base/shell.nix
@@ -9,6 +9,14 @@
     useBabelfish = true;
     shellAliases = lib.mkForce { };
     shellAbbrs = config.environment.shellAliases;
+    interactiveShellInit =
+      lib.mkIf (config.networking.hostName == "nixwsl") # fish
+        ''
+          function code
+            set code (which code)
+            $code $argv
+          end
+        '';
   };
 
   programs.command-not-found.enable = false;
