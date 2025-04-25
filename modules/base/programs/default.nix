80fede3b74a7ae9c6ebd040d7c3968d4887ceb78
Joaquín Guerra <45147327+warbacon@users.noreply.github.com>
2025-04-25 17:51:00 +0200

    programs: hide desktop entries

--- modules/base/programs/default.nix
+++ modules/base/programs/default.nix
@@ -38,4 +38,35 @@
     php
     phpPackages.composer
   ];
+
+  home-manager.users.warbacon.xdg.desktopEntries = {
+    nvim = {
+      name = "nvim";
+      noDisplay = true;
+    };
+    bottom = {
+      name = "bottom";
+      noDisplay = true;
+    };
+    nvtop = {
+      name = "nvtop";
+      noDisplay = true;
+    };
+    fish = {
+      name = "fish";
+      noDisplay = true;
+    };
+    uuctl = {
+      name = "uuctl";
+      noDisplay = true;
+    };
+    rofi = {
+      name = "rofi";
+      noDisplay = true;
+    };
+    rofi-theme-selector = {
+      name = "rofi-theme-selector";
+      noDisplay = true;
+    };
+  };
 }
