3f0bccb3c4c7abfe6dd1b004492fd7661e4a3b23
Joaquín Guerra <45147327+warbacon@users.noreply.github.com>
2025-04-28 12:54:04 +0200

    theming: use monospace instead of consolas

--- modules/graphical/theming.nix
+++ modules/graphical/theming.nix
@@ -12,6 +12,15 @@
 
   fonts.fontconfig = {
     enable = true;
+    localConf = # xml
+      ''
+        <alias>
+            <family>Consolas</family>
+            <prefer>
+              <family>monospace</family>
+            </prefer>
+        </alias>
+      '';
     defaultFonts = {
       emoji = [ "Noto Color Emoji" ];
       monospace = [
