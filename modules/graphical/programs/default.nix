2bae4f2c5d644fe6e04dbaed94efb5afa093786e
Joaquín Guerra <45147327+warbacon@users.noreply.github.com>
2025-04-30 23:39:20 +0200

    programs: localsend fix and vscode

--- modules/graphical/programs/default.nix
+++ modules/graphical/programs/default.nix
@@ -6,13 +6,19 @@
     ./kitty
   ];
 
+  # Enable Wayland in Chromium/Electron
+  environment.sessionVariables.NIXOS_OZONE_WL = "1";
+
+  # Needed to open firewall ports
+  programs.localsend.enable = true;
+
   # Packages
   environment.systemPackages = with pkgs; [
     chromium
     imv
     libreoffice-fresh
-    localsend
     mpv
+    vscode-fhs
     wl-clipboard
   ];
 }
