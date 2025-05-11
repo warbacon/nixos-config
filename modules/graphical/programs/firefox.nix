e4dd65f2ddf4fa267547df7531d9ab44e15cd394
Joaquín Guerra <45147327+warbacon@users.noreply.github.com>
2025-05-11 21:43:45 +0200

    firefox: add personal profile with linux especific settings

--- modules/graphical/programs/firefox.nix
+++ modules/graphical/programs/firefox.nix
@@ -12,5 +12,15 @@
       OverridePostUpdatePage = "";
       RequestedLocales = "es-ES";
     };
+    profiles = {
+      personal = {
+        settings = {
+          "apz.fling_friction" = 0.004;
+          "apz.gtk.pangesture.delta_mode" = 2;
+          "apz.gtk.pangesture.pixel_delta_mode_multiplier" = 25;
+          "middlemouse.paste" = false;
+        };
+      };
+    };
   };
 }
