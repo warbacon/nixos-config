45bf37b4b88dc2c6c4024c87756255f6cc1ad14c
Joaquín Guerra <45147327+warbacon@users.noreply.github.com>
2025-08-03 03:00:02 +0200

    flake: unify nixpkgs and home-manager inputs

--- flake.nix
+++ flake.nix
@@ -9,11 +9,15 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    nixos-wsl.url = "github:nix-community/NixOS-WSL/main";
+    nixos-wsl = {
+      url = "github:nix-community/NixOS-WSL/main";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
 
     zen-browser = {
       url = "github:0xc000022070/zen-browser-flake";
       inputs.nixpkgs.follows = "nixpkgs";
+      inputs.home-manager.follows = "home-manager";
     };
   };
   outputs =
