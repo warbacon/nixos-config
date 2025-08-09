f32035d63d49b143faf5fcc91f3ed760d25e1c1e
Joaquín Guerra <45147327+warbacon@users.noreply.github.com>
2025-08-09 12:16:41 +0200

    base: fix locale jiji

--- modules/base/default.nix
+++ modules/base/default.nix
@@ -56,18 +56,7 @@ in
   time.timeZone = "Europe/Madrid";
 
   # Locale
-  i18n.defaultLocale = "es_US.UTF-8";
-  i18n.extraLocaleSettings = {
-    LC_ADDRESS = "es_ES.UTF-8";
-    LC_IDENTIFICATION = "es_ES.UTF-8";
-    LC_MEASUREMENT = "es_ES.UTF-8";
-    LC_MONETARY = "es_ES.UTF-8";
-    LC_NAME = "es_ES.UTF-8";
-    LC_NUMERIC = "es_ES.UTF-8";
-    LC_PAPER = "es_ES.UTF-8";
-    LC_TELEPHONE = "es_ES.UTF-8";
-    LC_TIME = "es_ES.UTF-8";
-  };
+  i18n.defaultLocale = "es_ES.UTF-8";
 
   # Configure console keymap
   console.keyMap = "es";
