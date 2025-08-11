65b7909b38186d60f4b4bd7f300c23ee6c71194c
Joaquín Guerra <45147327+warbacon@users.noreply.github.com>
2025-08-11 13:16:52 +0200

    flake: don't ask permission on rebuild for using cachix

--- flake.nix
+++ flake.nix
@@ -1,15 +1,6 @@
 {
   description = "My personal NixOS configuration.";
 
-  nixConfig = {
-    extra-substituters = [
-      "https://nix-community.cachix.org"
-    ];
-    extra-trusted-public-keys = [
-      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-    ];
-  };
-
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
 
--- modules/base/default.nix
+++ modules/base/default.nix
@@ -52,6 +52,16 @@ in
     "flakes"
   ];
 
+  # Cachix binary cache
+  nix.settings = {
+    trusted-substituters = [
+      "https://nix-community.cachix.org"
+    ];
+    trusted-public-keys = [
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+    ];
+  };
+
   # Timezone
   time.timeZone = "Europe/Madrid";
 
