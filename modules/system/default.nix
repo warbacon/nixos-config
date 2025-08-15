a43493b6940eef96f31d73dd2fd22e9600a8b3fb
Joaquín Guerra <45147327+warbacon@users.noreply.github.com>
2025-08-15 20:32:04 +0200

    refactor: simpler system module

--- modules/system/default.nix
+++ modules/system/default.nix
@@ -1,11 +1,37 @@
 {
-  imports = [
-    ./adb.nix
-    ./bluetooth.nix
-    ./bootloader.nix
-    ./printing.nix
-    ./networking.nix
-  ];
+  pkgs,
+  lib,
+  config,
+  ...
+}:
+{
+  boot = {
+    kernelPackages = pkgs.linuxPackages_latest;
+    loader = {
+      timeout = 0;
+      systemd-boot.enable = true;
+    };
+  };
+
+  networking = {
+    networkmanager = {
+      enable = true;
+      insertNameservers = [
+        "193.110.81.0"
+        "185.253.5.0"
+      ];
+    };
+  };
+
+  services.printing = {
+    drivers = [ pkgs.hplipWithPlugin ];
+  };
 
   zramSwap.enable = true;
+  programs.adb.enable = true;
+
+  environment.systemPackages = lib.mkIf config.hardware.bluetooth.enable [
+    pkgs.bluetui
+    pkgs.ear2ctl
+  ];
 }
--- modules/system/printing.nix
+++
@@ -1,6 +0,0 @@
-{ pkgs, ... }:
-{
-  services.printing = {
-    drivers = [ pkgs.hplipWithPlugin ];
-  };
-}
