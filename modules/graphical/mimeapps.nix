37e737293cf5b5a3bbadc4b0859f70bbec7e635a
Joaquín Guerra <45147327+warbacon@users.noreply.github.com>
2025-08-23 23:56:30 +0200

    mimeapps: xdg-terminal-exec

--- modules/graphical/mimeapps.nix
+++ modules/graphical/mimeapps.nix
@@ -1,18 +1,27 @@
 {
-  home-manager.users.warbacon.xdg = {
-    mimeApps = {
+  xdg = {
+    terminal-exec = {
       enable = true;
-      defaultApplications = {
-        "application/pdf" = [ "zen-beta.desktop" ];
-        "text/html" = [ "zen-beta.desktop" ];
-        "x-scheme-handler/http" = [ "zen-beta.desktop" ];
-        "x-scheme-handler/https" = [ "zen-beta.desktop" ];
-        "x-scheme-handler/mailto" = [ "zen-beta.desktop" ];
-        "image/png" = [ "org.gnome.Loupe.desktop" ];
-        "image/jpeg" = [ "org.gnome.Loupe.desktop" ];
-        "image/gif" = [ "org.gnome.Loupe.desktop" ];
-        "inode/directory" = [ "org.gnome.Nautilus.desktop" ];
+      settings = {
+        default = [
+          "kitty.desktop"
+          "ghostty.desktop"
+          "footclient.desktop"
+          "alacritty.desktop"
+          "wezterm.desktop"
+        ];
       };
+    };
+    mime.defaultApplications = {
+      "application/pdf" = [ "zen-beta.desktop" ];
+      "text/html" = [ "zen-beta.desktop" ];
+      "x-scheme-handler/http" = [ "zen-beta.desktop" ];
+      "x-scheme-handler/https" = [ "zen-beta.desktop" ];
+      "x-scheme-handler/mailto" = [ "zen-beta.desktop" ];
+      "image/png" = [ "org.gnome.Loupe.desktop" ];
+      "image/jpeg" = [ "org.gnome.Loupe.desktop" ];
+      "image/gif" = [ "org.gnome.Loupe.desktop" ];
+      "inode/directory" = [ "org.gnome.Nautilus.desktop" ];
     };
   };
 }
