800e349fe7e698bb8a4186b2b68f647014290efb
Joaquín Guerra <45147327+warbacon@users.noreply.github.com>
2025-08-26 18:47:46 +0200

    gnome: improvements

--- modules/graphical/gnome.nix
+++ modules/graphical/gnome.nix
@@ -1,4 +1,9 @@
-{ pkgs, lib, ... }:
+{
+  pkgs,
+  lib,
+  config,
+  ...
+}:
 {
   services.displayManager.gdm.enable = true;
 
@@ -6,7 +11,11 @@
   services.gnome.core-apps.enable = false;
 
   services.gnome.evolution-data-server.enable = lib.mkForce false;
-  services.gnome.localsearch.enable = lib.mkForce false;
+  services.dleyna.enable = lib.mkForce false;
+  services.gnome.at-spi2-core.enable = lib.mkForce false;
+  services.gnome.gnome-initial-setup.enable = lib.mkForce false;
+  services.gnome.rygel.enable = lib.mkForce false;
+  services.hardware.bolt.enable = lib.mkForce false;
 
   environment.gnome.excludePackages = [
     pkgs.gnome-tour
@@ -14,9 +23,29 @@
   ];
 
   services.gnome.sushi.enable = true;
+
+  home-manager.users.warbacon = {
+    gtk = {
+      enable = true;
+      gtk3.theme = {
+        name = "Adwaita-dark";
+        package = pkgs.gnome-themes-extra;
+      };
+      gtk2.configLocation = "${config.home-manager.users.warbacon.xdg.configHome}/gtk-2.0/gtkrc";
+    };
+
+    dconf.settings = {
+      "org/gnome/desktop/interface" = {
+        color-scheme = "prefer-dark";
+      };
+    };
+  };
+
   environment.systemPackages = [
-    pkgs.file-roller
+    pkgs.gnomeExtensions.appindicator
     pkgs.gnomeExtensions.pano
+
+    pkgs.gnome-characters
     pkgs.nautilus
   ];
 }
