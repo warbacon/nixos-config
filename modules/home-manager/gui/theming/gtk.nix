cf4fed819d643df419051d449d73a917334da341
Joaquín Guerra <45147327+warbacon@users.noreply.github.com>
2025-08-27 10:28:23 +0200

    gtk: oops

--- modules/home-manager/gui/theming/gtk.nix
+++ modules/home-manager/gui/theming/gtk.nix
@@ -1,8 +1,4 @@
 { config, pkgs, ... }:
-let
-  homeDir = config.home.homeDirectory;
-
-in
 {
   gtk = {
     enable = true;
@@ -10,7 +6,7 @@ in
       name = "Adwaita-dark";
       package = pkgs.gnome-themes-extra;
     };
-    gtk2.configLocation = "${homeDir}/gtk-2.0/gtkrc";
+    gtk2.configLocation = "${config.xdg.configHome}/gtk-2.0/gtkrc";
   };
 
   dconf.settings = {
