79d2933c921b891053bec0d66bc6a59f669705b1
Joaquín Guerra <45147327+warbacon@users.noreply.github.com>
2025-08-28 16:33:28 +0200

    kitty: concat theme files to config

--- modules/home-manager/gui/kitty/default.nix
+++ modules/home-manager/gui/kitty/default.nix
@@ -1,13 +1,13 @@
+{ lib, ... }:
 {
   programs.kitty = {
     enable = true;
     enableGitIntegration = true;
     shellIntegration.mode = null;
-    extraConfig = builtins.readFile ./kitty.conf;
-  };
-
-  xdg.configFile."kitty/themes" = {
-    source = ./themes;
-    recursive = true;
+    extraConfig = lib.concatStrings [
+      (builtins.readFile ./kitty.conf)
+      "\n# COLORSCHEME\n"
+      (builtins.readFile ./themes/tokyonight.conf)
+    ];
   };
 }
