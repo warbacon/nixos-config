6eafc69e2bcf38eb774f9882a06c0c3af93508c5
Joaquín Guerra <45147327+warbacon@users.noreply.github.com>
2025-08-28 16:34:08 +0200

    zen: POLICIES 👮
    No espera así no era

--- modules/home-manager/gui/zen.nix
+++ modules/home-manager/gui/zen.nix
@@ -7,7 +7,15 @@
   programs.zen-browser = {
     enable = true;
     policies = {
+      AutofillAddressEnabled = false;
+      AutofillCreditCardEnabled = false;
       DisableAppUpdate = true;
+      DisableFeedbackCommands = true;
+      DisableFirefoxStudies = true;
+      DisableTelemetry = true;
+      DontCheckDefaultBrowser = true;
+      NoDefaultBookmarks = true;
+      OfferToSaveLogins = false;
     };
   };
 }
