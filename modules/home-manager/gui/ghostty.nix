2a58a50c99c14878d6325d1e984b9a189b6196ec
Joaquín Guerra <45147327+warbacon@users.noreply.github.com>
2025-09-19 22:42:03 +0200

    ghostty: use nixpkgs version

--- modules/home-manager/gui/ghostty.nix
+++ modules/home-manager/gui/ghostty.nix
@@ -1,9 +1,6 @@
-{ pkgs, inputs, ... }:
 {
   programs.ghostty = {
     enable = true;
-    package = inputs.ghostty.packages.${pkgs.stdenv.hostPlatform.system}.default;
-    installVimSyntax = true;
     settings = {
       # Shell
       command = "fish";
