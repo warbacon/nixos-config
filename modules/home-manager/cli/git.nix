0c91b51870d2a8631b10f02843d035c90a420b27
Joaquín Guerra <45147327+warbacon@users.noreply.github.com>
2025-10-15 21:54:08 +0200

    git: set neovim as difftool

--- modules/home-manager/cli/git.nix
+++ modules/home-manager/cli/git.nix
@@ -6,6 +6,10 @@
     userEmail = "45147327+warbacon@users.noreply.github.com";
     extraConfig = {
       init.defaultBranch = "main";
+      diff.tool = "nvim_difftool";
+      difftool."nvim_difftool".cmd = ''
+        nvim -c "packadd nvim.difftool" -c "DiffTool $LOCAL $REMOTE"
+      '';
     };
   };
 
