f69d6e685a5a378b0246c6ce5a0edc92bd03133c
Joaquín Guerra <45147327+warbacon@users.noreply.github.com>
2025-11-04 23:31:53 +0100

    qt: fancy qt theming

--- modules/home-manager/gui/theming/qt.nix
+++ modules/home-manager/gui/theming/qt.nix
@@ -1,50 +1,60 @@
 {
-  pkgs,
   lib,
+  pkgs,
   osConfig,
   ...
 }:
+let
+  Kvantum-Tokyo-Night = pkgs.fetchFromGitHub {
+    owner = "0xsch1zo";
+    repo = "Kvantum-Tokyo-Night";
+    rev = "82d104e0047fa7d2b777d2d05c3f22722419b9ee";
+    hash = "sha256-Uy/WthoQrDnEtrECe35oHCmszhWg38fmDP8fdoXQgTk=";
+    sparseCheckout = [ "Kvantum-Tokyo-Night" ];
+  };
+
+  qtctConf = {
+    Appearance = {
+      custom_palette = false;
+      icon_theme = "breeze-dark";
+      standard_dialogs = "xdgdesktopportal";
+      style = "kvantum";
+    };
+    Fonts = {
+      fixed = "\"monospace,10,-1,5,50,0,0,0,0,0,Regular\"";
+      general = "\"sans-serif,10,-1,5,50,0,0,0,0,0\"";
+    };
+    Interface = {
+      underline_shortcut = 0;
+    };
+  };
+in
 {
   config = lib.mkIf (osConfig.this.desktop != "plasma") {
-    home.packages = [ pkgs.kdePackages.breeze-icons ];
-
     qt = {
       enable = true;
       platformTheme.name = "qtct";
     };
 
-    xdg.configFile."qt6ct/qt6ct.conf".text = lib.generators.toINI { } {
-      Appearance = {
-        color_scheme_path = "${pkgs.kdePackages.qt6ct}/share/qt6ct/colors/darker.conf";
-        custom_palette = true;
-        icon_theme = "breeze-dark";
-      };
-      Fonts = {
-        fixed = "\"monospace,10,-1,5,50,0,0,0,0,0,Regular\"";
-        general = "\"sans-serif,10,-1,5,50,0,0,0,0,0\"";
-      };
-      Interface = {
-        dialog_buttons_have_icons = 0;
-        toolbutton_style = 0;
-        underline_shortcut = 0;
-      };
-    };
+    home.packages = [
+      pkgs.qt6Packages.qtstyleplugin-kvantum
+      pkgs.libsForQt5.qtstyleplugin-kvantum
+      pkgs.libsForQt5.qt5ct
+      pkgs.kdePackages.breeze-icons
+    ];
 
-    xdg.configFile."qt5ct/qt5ct.conf".text = lib.generators.toINI { } {
-      Appearance = {
-        color_scheme_path = "${pkgs.libsForQt5.qt5ct}/share/qt5ct/colors/darker.conf";
-        custom_palette = true;
-        icon_theme = "breeze-dark";
+    xdg.configFile = {
+      "Kvantum/Kvantum-Tokyo-Night" = {
+        source = "${Kvantum-Tokyo-Night}/Kvantum-Tokyo-Night";
+        recursive = true;
       };
-      Fonts = {
-        fixed = "\"monospace,10,-1,5,50,0,0,0,0,0,Regular\"";
-        general = "\"sans-serif,10,-1,5,50,0,0,0,0,0\"";
-      };
-      Interface = {
-        dialog_buttons_have_icons = 0;
-        toolbutton_style = 0;
-        underline_shortcut = 0;
-      };
+      "Kvantum/kvantum.kvconfig".text = ''
+        [General]
+        theme=Kvantum-Tokyo-Night
+      '';
+
+      "qt6ct/qt6ct.conf".text = lib.generators.toINI { } qtctConf;
+      "qt5ct/qt5ct.conf".text = lib.generators.toINI { } qtctConf;
     };
   };
 }
