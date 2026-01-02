fbadffb6f0a73e95c1a736e583352033ccccc613
Joaquín Guerra <joaquinguerratocino@gmail.com>
2026-01-02 21:56:34 +0100

    feat(fish): make wsdl work in nixos

--- home-manager/fish/functions/wsdl.fish
+++ home-manager/fish/functions/wsdl.fish
@@ -1,4 +1,8 @@
 function wsdl --description "Run SDL applications with Wayland video driver"
     set -x SDL_VIDEODRIVER wayland
-    command $argv[1]
+    if test -d /nix
+        steam-run $argv[1]
+    else
+        command $argv[1]
+    end
 end
