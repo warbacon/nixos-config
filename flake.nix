64489dfaf38378ebd0662c5033daf0165c7d5709
Joaquín Guerra <joaquinguerratocino@gmail.com>
2026-04-13 18:52:18 +0200

    feat: use zen-browser flake recommended in wiki
    It's smaller and enough

--- flake.nix
+++ flake.nix
@@ -5,7 +5,7 @@
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
 
     zen-browser = {
-      url = "github:0xc000022070/zen-browser-flake";
+      url = "github:youwen5/zen-browser-flake";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
