1754ad4c87bcaf3b3a094a7a44e140d8fec3314c
Joaquín Guerra <joaquinguerratocino@gmail.com>
2026-06-13 22:47:30 +0200

    feat(qt): breeze-dark working (@SeniorMatt -> GOAT)

--- modules/gui/qt.nix
+++ modules/gui/qt.nix
@@ -1,28 +1,29 @@
 { pkgs, ... }:
 {
   home-manager.users.warbacon = {
-    home.packages = [
-      pkgs.kdePackages.breeze-icons
+    home.packages = with pkgs; [
+      kdePackages.breeze-icons
     ];
+
     qt = {
       enable = true;
-      platformTheme.name = "qtct";
-      qt6ctSettings = {
-        Appearance = {
-          color_scheme_path = "${pkgs.qt6Packages.qt6ct}/share/qt6ct/colors/darker.conf";
-          icon_theme = "breeze-dark";
-          standard_dialogs = "xdgdesktopportal";
-          custom_palette = true;
-          style = "Fusion";
-        };
-        Fonts = {
-          fixed = "\"Monospace,10,-1,0,400,0,0,0,0,0,0,0,0,0,0,1,,0,0\"";
-          general = "\"Sans Serif,10,-1,0,400,0,0,0,0,0,0,0,0,0,0,1,,0,0\"";
-        };
-        Interface = {
-          toolbutton_style = 0;
-        };
-      };
+      platformTheme.name = "kde";
+      style.name = "breeze";
     };
+
+    home.file.".config/kdeglobals".text = ''
+      ${builtins.readFile "${pkgs.kdePackages.breeze}/share/color-schemes/BreezeDark.colors"}
+
+      [General]
+      font=Sans Serif,10,-1,5,50,0,0,0,0,0
+      menuFont=Sans Serif,10,-1,5,50,0,0,0,0,0
+      windowFont=Sans Serif,10,-1,5,50,0,0,0,0,0
+      toolbarFont=Sans Serif,10,-1,5,50,0,0,0,0,0
+      smallestReadableFont=Sans Serif,8,-1,5,50,0,0,0,0,0
+      fixed=Monospace,10,-1,5,50,0,0,0,0,0
+
+      [Icons]
+      Theme=breeze-dark
+    '';
   };
 }
