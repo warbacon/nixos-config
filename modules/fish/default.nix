1164a30e4a8670d86ed94450047c88c911316301
Joaquín Guerra <joaquinguerratocino@gmail.com>
2026-06-18 21:22:53 +0200

    fix(fish): use default selected background

--- modules/fish/default.nix
+++ modules/fish/default.nix
@@ -18,7 +18,6 @@
           set fish_color_param --reset
           set fish_color_command cyan
           set fish_pager_color_progress brblack --italic
-          set fish_pager_color_selected_background -b "2b3b51"
           fish_add_path -P ${config.home-manager.users.warbacon.programs.npm.settings.prefix}/bin
           fzf --fish | source
         '';
