044101bf6503b32a4fba85a975a3de54e356d2a4
Joaquín Guerra <joaquinguerratocino@gmail.com>
2026-06-30 20:03:56 +0200

    feat(fish): reset fish operator syntax color

--- modules/fish/default.nix
+++ modules/fish/default.nix
@@ -16,6 +16,7 @@
         ''
           set fish_greeting
           set fish_color_param --reset
+          set fish_color_operator --reset
           set fish_color_command cyan --bold
           set fish_pager_color_progress brblack --italic
           fish_add_path -P ${config.home-manager.users.warbacon.programs.npm.settings.prefix}/bin
